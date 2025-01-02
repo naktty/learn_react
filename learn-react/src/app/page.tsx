9c4fb13acbcbc61a2384ca95590e6709da4c22a5
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-01-02 12:09:34 +0900

    波括弧を使ってJSX 内でJavaScript変数を参照する

--- learn-react/src/app/page.tsx
+++ learn-react/src/app/page.tsx
@@ -1,11 +1,23 @@
-import Gallery from "./ui/gallery";
-import Profile from "./ui/profile";
+// import Gallery from "./ui/gallery";
+// import Profile from "./ui/profile";
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <>
+//       <Profile />
+//       <Gallery />
+//     </>
+//   );
+// }
+
+export default function Avatar() {
+  const avatar = 'https://i.imgur.com/7vQD0fPs.jpg';
+  const description = 'Gregorio Y. Zara';
   return (
-    <>
-      <Profile />
-      <Gallery />
-    </>
+    <img
+      className="avatar"
+      src={avatar}
+      alt={description}
+    />
   );
 }
