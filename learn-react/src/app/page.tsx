9725a98d555f917714236a473386984cab961552
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-01-03 21:31:16 +0900

    state 変数の追加

--- learn-react/src/app/page.tsx
+++ learn-react/src/app/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import { sculptureList } from './lib/data';
+import { useState } from 'react';
 
 export default function Gallery() {
-  let index = 0;
+  const [index, setIndex] = useState(0);
 
   function handleClick() {
-    index = index + 1;
+    setIndex(index + 1);
   }
 
   let sculpture = sculptureList[index];
