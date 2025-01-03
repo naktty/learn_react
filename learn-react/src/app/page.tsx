db4ddd6bfa0acc785cd251a899da71349ca122f5
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-01-03 16:53:08 +0900

    イベントハンドラの props の命名

--- learn-react/src/app/page.tsx
+++ learn-react/src/app/page.tsx
@@ -1,38 +1,31 @@
 'use client';
 
+export default function App() {
+  return (
+    <Toolbar
+      onPlayMovie={() => alert('Playing!')}
+      onUploadImage={() => alert('Uploading!')}
+    />
+  );
+}
+
+function Toolbar({ onPlayMovie, onUploadImage }) {
+  return (
+    <div>
+      <Button onClick={onPlayMovie}>
+        Play Movie
+      </Button>
+      <Button onClick={onUploadImage}>
+        Upload Image
+      </Button>
+    </div>
+  );
+}
+
 function Button({ onClick, children }) {
   return (
     <button onClick={onClick}>
       {children}
     </button>
-  );
-}
-
-function PlayButton({ movieName }) {
-  function handlePlayClick() {
-    alert(`Playing ${movieName}!`);
-  }
-
-  return (
-    <Button onClick={handlePlayClick}>
-      Play "{movieName}"
-    </Button>
-  );
-}
-
-function UploadButton() {
-  return (
-    <Button onClick={() => alert('Uploading!')}>
-      Upload Image
-    </Button>
-  );
-}
-
-export default function Toolbar() {
-  return (
-    <div>
-      <PlayButton movieName="Kiki's Delivery Service" />
-      <UploadButton />
-    </div>
   );
 }
