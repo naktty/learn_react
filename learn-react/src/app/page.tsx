baa3db30abd037a50cf34fe894cec9ba67db6371
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-01-03 16:35:48 +0900

    イベントハンドラでの props の読み取り

--- learn-react/src/app/page.tsx
+++ learn-react/src/app/page.tsx
@@ -1,13 +1,22 @@
 'use client';
 
-export default function Button() {
-  function handleClick() {
-    alert('You clicked me!');
-  }
-
+function AlertButton({ message, children }) {
   return (
-    <button onClick={handleClick}>
-      Click me
+    <button onClick={() => alert(message)}>
+      {children}
     </button>
+  );
+}
+
+export default function Toolbar() {
+  return (
+    <div>
+      <AlertButton message="Playing!">
+        Play Movie
+      </AlertButton>
+      <AlertButton message="Uploading!">
+        Upload Image
+      </AlertButton>
+    </div>
   );
 }
