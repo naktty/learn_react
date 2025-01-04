fef4b748451e3fd0c5d96173ded238ab2ffbdd0c
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-01-04 09:27:23 +0900

    レンダーは時間を切り取ってスナップショットを取る

--- learn-react/src/app/page.tsx
+++ learn-react/src/app/page.tsx
@@ -2,28 +2,17 @@
 
 import { useState } from 'react';
 
-export default function Form() {
-  const [isSent, setIsSent] = useState(false);
-  const [message, setMessage] = useState('Hi!');
-  if (isSent) {
-    return <h1>Your message is on its way!</h1>
-  }
-  return (
-    <form onSubmit={(e) => {
-      e.preventDefault();
-      setIsSent(true);
-      sendMessage(message);
-    }}>
-      <textarea
-        placeholder="Message"
-        value={message}
-        onChange={e => setMessage(e.target.value)}
-      />
-      <button type="submit">Send</button>
-    </form>
-  );
-}
+export default function Counter() {
+  const [number, setNumber] = useState(0);
 
-function sendMessage(message) {
-  // ...
+  return (
+    <>
+      <h1>{number}</h1>
+      <button onClick={() => {
+        setNumber(number + 1);
+        setNumber(number + 1);
+        setNumber(number + 1);
+      }}>+3</button>
+    </>
+  )
 }
