46b0acc38f8be7de147fa8062285d0c61fa7b7b3
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-01-04 11:16:38 +0900

    時間経過と state

--- learn-react/src/app/page.tsx
+++ learn-react/src/app/page.tsx
@@ -2,17 +2,34 @@
 
 import { useState } from 'react';
 
-export default function Counter() {
-  const [number, setNumber] = useState(0);
+export default function Form() {
+  const [to, setTo] = useState('Alice');
+  const [message, setMessage] = useState('Hello');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTimeout(() => {
+      alert(`You said ${message} to ${to}`);
+    }, 5000);
+  }
 
   return (
-    <>
-      <h1>{number}</h1>
-      <button onClick={() => {
-        setNumber(number + 1);
-        setNumber(number + 1);
-        setNumber(number + 1);
-      }}>+3</button>
-    </>
-  )
+    <form onSubmit={handleSubmit}>
+      <label>
+        To:{' '}
+        <select
+          value={to}
+          onChange={e => setTo(e.target.value)}>
+          <option value="Alice">Alice</option>
+          <option value="Bob">Bob</option>
+        </select>
+      </label>
+      <textarea
+        placeholder="Message"
+        value={message}
+        onChange={e => setMessage(e.target.value)}
+      />
+      <button type="submit">Send</button>
+    </form>
+  );
 }
