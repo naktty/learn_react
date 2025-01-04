0f2deaca249f707887599c813cb52aa1264f554c
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-01-04 17:04:15 +0900

    配列に要素を追加

--- learn-react/src/app/page.tsx
+++ learn-react/src/app/page.tsx
@@ -2,57 +2,30 @@
 
 import { useState } from 'react';
 
-export default function Scoreboard() {
-  const [player, setPlayer] = useState({
-    firstName: 'Ranjani',
-    lastName: 'Shettar',
-    score: 10,
-  });
+let nextId = 0;
 
-  function handlePlusClick() {
-    setPlayer({
-      ...player,
-      score: player.score + 1,
-    });
-  }
-
-  function handleFirstNameChange(e) {
-    setPlayer({
-      ...player,
-      firstName: e.target.value,
-    });
-  }
-
-  function handleLastNameChange(e) {
-    setPlayer({
-      ...player,
-      lastName: e.target.value
-    });
-  }
+export default function List() {
+  const [name, setName] = useState('');
+  const [artists, setArtists] = useState([]);
 
   return (
     <>
-      <label>
-        Score: <b>{player.score}</b>
-        {' '}
-        <button onClick={handlePlusClick}>
-          +1
-        </button>
-      </label>
-      <label>
-        First name:
-        <input
-          value={player.firstName}
-          onChange={handleFirstNameChange}
-        />
-      </label>
-      <label>
-        Last name:
-        <input
-          value={player.lastName}
-          onChange={handleLastNameChange}
-        />
-      </label>
+      <h1>Inspiring sculptors:</h1>
+      <input
+        value={name}
+        onChange={e => setName(e.target.value)}
+      />
+      <button onClick={() => {
+        setArtists([
+          ...artists,
+          { id: nextId++, name: name }
+        ]);
+      }}>Add</button>
+      <ul>
+        {artists.map(artist => (
+          <li key={artist.id}>{artist.name}</li>
+        ))}
+      </ul>
     </>
   );
 }
