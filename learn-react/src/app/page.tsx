08b0e01c37ceedc5cf5af0cd23946a9bb3d4b7a7
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-01-09 20:49:54 +0900

    チャレンジ 1/4

--- learn-react/src/app/page.tsx
+++ learn-react/src/app/page.tsx
@@ -2,76 +2,51 @@
 
 import { useState } from 'react';
 
-let nextId = 3;
-const initialList = [
-  { id: 0, title: 'Big Bellies', seen: false },
-  { id: 1, title: 'Lunar Landscape', seen: false },
-  { id: 2, title: 'Terracotta Army', seen: true },
-];
+const initialProducts = [{
+  id: 0,
+  name: 'Baklava',
+  count: 1,
+}, {
+  id: 1,
+  name: 'Cheese',
+  count: 5,
+}, {
+  id: 2,
+  name: 'Spaghetti',
+  count: 2,
+}];
 
-export default function BucketList() {
-  const [myList, setMyList] = useState(initialList);
-  const [yourList, setYourList] = useState(
-    initialList
-  );
+export default function ShoppingCart() {
+  const [
+    products,
+    setProducts
+  ] = useState(initialProducts)
 
-  function handleToggleMyList(artworkId, nextSeen) {
-    setMyList(myList.map(artwork => {
-      if (artwork.id === artworkId) {
-        // Create a *new* object with changes
-        return { ...artwork, seen: nextSeen };
+  function handleIncreaseClick(productId) {
+    setProducts(products.map(product => {
+      if (product.id === productId) {
+        return {
+          ...product,
+          count: product.count + 1
+        };
       } else {
-        // No changes
-        return artwork;
+        return product;
       }
-    }));
+    }))
   }
 
-  function handleToggleYourList(artworkId, nextSeen) {
-    setYourList(yourList.map(artwork => {
-      if (artwork.id === artworkId) {
-        // Create a *new* object with changes
-        return { ...artwork, seen: nextSeen };
-      } else {
-        // No changes
-        return artwork;
-      }
-    }));
-  }
-
-  return (
-    <>
-      <h1>Art Bucket List</h1>
-      <h2>My list of art to see:</h2>
-      <ItemList
-        artworks={myList}
-        onToggle={handleToggleMyList} />
-      <h2>Your list of art to see:</h2>
-      <ItemList
-        artworks={yourList}
-        onToggle={handleToggleYourList} />
-    </>
-  );
-}
-
-function ItemList({ artworks, onToggle }) {
   return (
     <ul>
-      {artworks.map(artwork => (
-        <li key={artwork.id}>
-          <label>
-            <input
-              type="checkbox"
-              checked={artwork.seen}
-              onChange={e => {
-                onToggle(
-                  artwork.id,
-                  e.target.checked
-                );
-              }}
-            />
-            {artwork.title}
-          </label>
+      {products.map(product => (
+        <li key={product.id}>
+          {product.name}
+          {' '}
+          (<b>{product.count}</b>)
+          <button onClick={() => {
+            handleIncreaseClick(product.id);
+          }}>
+            +
+          </button>
         </li>
       ))}
     </ul>
