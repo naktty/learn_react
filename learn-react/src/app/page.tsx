f6872204458f309de76498a9c4af91f732ddebb8
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-01-11 22:28:36 +0900

    チャレンジ問題2(自分の回答)

--- learn-react/src/app/page.tsx
+++ learn-react/src/app/page.tsx
@@ -2,70 +2,47 @@
 
 import { useState } from 'react';
 
-export default function Form() {
-  const [answer, setAnswer] = useState('');
-  const [error, setError] = useState(null);
-  const [status, setStatus] = useState('typing');
-
-  if (status === 'success') {
-    return <h1>That's right!</h1>
-  }
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-    setStatus('submitting');
-    try {
-      await submitForm(answer);
-      setStatus('success');
-    } catch (err) {
-      setStatus('typing');
-      setError(err);
-    }
-  }
-
-  function handleTextareaChange(e) {
-    setAnswer(e.target.value);
-  }
-
+export default function EditProfile() {
+  const[mode, setMode] = useState('Browse');
+  const[firstName, setFirstName] = useState('Jane');
+  const[lastName, setLastName] = useState('Jacobs');
+  
   return (
-    <>
-      <h2>City quiz</h2>
-      <p>
-        In which city is there a billboard that turns air into drinkable water?
-      </p>
-      <form onSubmit={handleSubmit}>
-        <textarea
-          value={answer}
-          onChange={handleTextareaChange}
-          disabled={status === 'submitting'}
-        />
-        <br />
-        <button disabled={
-          answer.length === 0 ||
-          status === 'submitting'
-        }>
-          Submit
-        </button>
-        {error !== null &&
-          <p className="Error">
-            {error.message}
-          </p>
+    <form>
+      <label>
+        First name:{' '}
+        {mode==='Edit' ?
+          <input
+            value={firstName}
+            onChange={e => {
+              setFirstName(e.target.value);
+            }}
+          />
+          : <b>{firstName}</b>
         }
-      </form>
-    </>
+      </label>
+      <label>
+        Last name:{' '}
+        {mode==='Edit' ?
+          <input
+            value={lastName}
+            onChange={e => {
+              setLastName(e.target.value);
+            }}
+          />
+          : <b>{lastName}</b>
+        }
+      </label>
+      <button
+        type="submit"
+        onClick={e => {
+          e.preventDefault();
+          mode==='Edit' ? setMode('Browse') : setMode('Edit');
+        }}
+      >
+        {mode === 'Browse' ? 'Edit Profile' : 'Save Profile'}
+      </button>
+      <p><i>{`Hello, ${firstName} ${lastName}!`}</i></p>
+    </form>
   );
-}
-
-function submitForm(answer) {
-  // Pretend it's hitting the network.
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      let shouldError = answer.toLowerCase() !== 'lima'
-      if (shouldError) {
-        reject(new Error('Good guess but a wrong answer. Try again!'));
-      } else {
-        resolve();
-      }
-    }, 1500);
-  });
 }
