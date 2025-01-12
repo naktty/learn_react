56ca1e92dcadd1573a0f815996e6f4f208637030
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-01-12 11:13:44 +0900

    チャレンジ問題2(自分の回答)

--- learn-react/src/app/page.tsx
+++ learn-react/src/app/page.tsx
@@ -1,47 +1,61 @@
 'use client';
 
 import { useState } from 'react';
+import AddItem from './AddItem.js';
+import PackingList from './PackingList.js';
 
-export default function EditProfile() {
-  const [isEditing, setIsEditing] = useState(false);
-  const [firstName, setFirstName] = useState('Jane');
-  const [lastName, setLastName] = useState('Jacobs');
+let nextId = 3;
+const initialItems = [
+  { id: 0, title: 'Warm socks', packed: true },
+  { id: 1, title: 'Travel journal', packed: false },
+  { id: 2, title: 'Watercolors', packed: false },
+];
+
+export default function TravelPlan() {
+  const [items, setItems] = useState(initialItems);
+  const packed = items.filter(item =>
+    item.packed === true
+  );
+
+  function handleAddItem(title) {
+    setItems([
+      ...items,
+      {
+        id: nextId++,
+        title: title,
+        packed: false
+      }
+    ]);
+  }
+
+  function handleChangeItem(nextItem) {
+    setItems(items.map(item => {
+      if (item.id === nextItem.id) {
+        return nextItem;
+      } else {
+        return item;
+      }
+    }));
+  }
+
+  function handleDeleteItem(itemId) {
+    setItems(
+      items.filter(item => item.id !== itemId)
+    );
+  }
 
   return (
-    <form onSubmit={e => {
-      e.preventDefault();
-      setIsEditing(!isEditing);
-    }}>
-      <label>
-        First name:{' '}
-        {isEditing ? (
-          <input
-            value={firstName}
-            onChange={e => {
-              setFirstName(e.target.value)
-            }}
-          />
-        ) : (
-          <b>{firstName}</b>
-        )}
-      </label>
-      <label>
-        Last name:{' '}
-        {isEditing ? (
-          <input
-            value={lastName}
-            onChange={e => {
-              setLastName(e.target.value)
-            }}
-          />
-        ) : (
-          <b>{lastName}</b>
-        )}
-      </label>
-      <button type="submit">
-        {isEditing ? 'Save' : 'Edit'} Profile
-      </button>
-      <p><i>Hello, {firstName} {lastName}!</i></p>
-    </form>
+    <>  
+      <AddItem
+        onAddItem={handleAddItem}
+      />
+      <PackingList
+        items={items}
+        onChangeItem={handleChangeItem}
+        onDeleteItem={handleDeleteItem}
+      />
+      <hr />
+      <b>{packed.length} out of {items.length} packed!</b>
+    </>
   );
 }
