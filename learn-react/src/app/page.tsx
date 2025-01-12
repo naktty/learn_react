0ce63d5ed78475c716e9aa16d27f01776719942b
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-01-12 17:05:03 +0900

    チャレンジ問題2(自分の回答)

--- learn-react/src/app/page.tsx
+++ learn-react/src/app/page.tsx
@@ -1,39 +1,48 @@
 'use client';
 
 import { useState } from 'react';
+import { foods, filterItems } from './data.js';
 
-export default function SyncedInputs() {
-  const [text, setText] = useState('');
-
+export default function FilterableList() {
+  const [query, setQuery] = useState('');
+  const filteredFoods = filterItems(foods, query);
+  
   function handleChange(e) {
-    setText(e.target.value);
+    setQuery(e.target.value);
   }
-
+  
   return (
     <>
-      <Input
-        label="First input"
-        value={text}
-        onChange={handleChange}
-      />
-      <Input
-        label="Second input"
-        value={text}
-        onChange={handleChange}
-      />
+      <SearchBar query={query} onChange={handleChange} />
+      <hr />
+      <List items={filteredFoods} />
     </>
   );
 }
 
-function Input({ label, value, onChange }) {
+function SearchBar({query, onChange}) {
   return (
     <label>
-      {label}
-      {' '}
+      Search:{' '}
       <input
-        value={value}
+        value={query}
         onChange={onChange}
       />
     </label>
+  );
+}
+
+function List({ items }) {
+  return (
+    <table>
+      <tbody>
+        {items.map(food => (
+          <tr key={food.id}>
+            <td>{food.name}</td>
+            <td>{food.description}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
