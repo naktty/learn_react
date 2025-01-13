087bf518e2a411c398201c9d182c176e38fedeee
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-01-13 15:18:25 +0900

    チャレンジ問題1(模範回答)

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ import { useState } from 'react';
 
 export default function App() {
   const [showHint, setShowHint] = useState(false);
-  
   return (
     <div>
-      {showHint && <p><i>Hint: Your favorite city?</i></p>}
+      {showHint &&
+        <p><i>Hint: Your favorite city?</i></p>
+      }
       <Form />
-      {showHint
-      ? <button onClick={() => {
-        setShowHint(false);
-      }}>Hide hint</button>
-      : <button onClick={() => {
-        setShowHint(true);
-      }}>Show hint</button>}
+      {showHint ? (
+        <button onClick={() => {
+          setShowHint(false);
+        }}>Hide hint</button>
+      ) : (
+        <button onClick={() => {
+          setShowHint(true);
+        }}>Show hint</button>
+      )}
     </div>
   );
 }
