6931455b3525e04178ddb0f0f5f6b706af94863a
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-01-19 12:18:35 +0900

    チャレンジ問題2(自分の回答＝模範回答)

--- src/App.js
+++ src/App.js
@@ -1,42 +1,15 @@
 import './App.css';
 
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 
-export default function Chat() {
-  const [text, setText] = useState('');
-  const [isSending, setIsSending] = useState(false);
-  const timeoutRef = useRef(null);
-
-  function handleSend() {
-    setIsSending(true);
-    timeoutRef.current = setTimeout(() => {
-      alert('Sent!');
-      setIsSending(false);
-    }, 3000);
-  }
-
-  function handleUndo() {
-    setIsSending(false);
-    clearTimeout(timeoutRef.current);
-  }
+export default function Toggle() {
+  const[isOn, setIsOn] = useState(false);
 
   return (
-    <>
-      <input
-        disabled={isSending}
-        value={text}
-        onChange={e => setText(e.target.value)}
-      />
-      <button
-        disabled={isSending}
-        onClick={handleSend}>
-        {isSending ? 'Sending...' : 'Send'}
-      </button>
-      {isSending &&
-        <button onClick={handleUndo}>
-          Undo
-        </button>
-      }
-    </>
+    <button onClick={() => {
+      setIsOn(!isOn);
+    }}>
+      {isOn ? 'On' : 'Off'}
+    </button>
   );
 }
