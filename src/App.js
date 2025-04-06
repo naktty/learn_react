a69bb4429f7bdf2c03fcd62e89fd72dbd89a2dfd
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-04-06 22:14:28 +0900

    チャレンジ問題1（模範回答）

--- src/App.js
+++ src/App.js
@@ -4,35 +4,31 @@ import { useState, useRef } from 'react';
 
 export default function VideoPlayer() {
   const [isPlaying, setIsPlaying] = useState(false);
-  const videoRef = useRef(null);
+  const ref = useRef(null);
 
   function handleClick() {
     const nextIsPlaying = !isPlaying;
     setIsPlaying(nextIsPlaying);
-    
-    videoRef.current.controls = true;
+
     if (nextIsPlaying) {
-      videoRef.current.play();
+      ref.current.play();
     } else {
-      videoRef.current.pause();
+      ref.current.pause();
     }
   }
-
-  videoRef.onplay = (event) => {
-    setIsPlaying(false);
-  };
-
-  videoRef.onpause  = (event) => {
-    setIsPlaying(true);
-  };
 
   return (
     <>
       <button onClick={handleClick}>
         {isPlaying ? 'Pause' : 'Play'}
       </button>
-      <video ref={videoRef} width="250">
-        <source 
+      <video
+        width="250"
+        ref={ref}
+        onPlay={() => setIsPlaying(true)}
+        onPause={() => setIsPlaying(false)}
+      >
+        <source
           src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
           type="video/mp4"
         />
