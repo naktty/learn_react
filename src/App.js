773a866acc1381046a44b3847d66024020829f61
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-04-06 23:14:53 +0900

    チャレンジ問題3（自分の回答）

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
 import './App.css';
-import './referencing-values-with-refs/challenges3.css';
-import { useRef } from 'react';
+import './manipulating-the-dom-with-refs/challenges3.css';
+import { useState, useRef } from 'react';
 
-export default function Page() {
-  const inputRef = useRef(null);
+export default function CatFriends() {
+  const [index, setIndex] = useState(0);
+  const ref = useRef(null);
+
   return (
     <>
       <nav>
         <button onClick={() => {
-          inputRef.current.focus();
+          if (index < catList.length - 1) {
+            setIndex(index + 1);
+            ref.current.scrollIntoView({
+              behavior: 'smooth',
+              block: 'nearest',
+              inline: 'center'
+            });
+          } else {
+            setIndex(0);
+          }
         }}>
-          Search
+          Next
         </button>
       </nav>
-      <input
-        ref={inputRef}
-        placeholder="Looking for something?"
-      />
+      <div>
+        <ul>
+          {catList.map((cat, i) => (
+            <li
+              key={cat.id}
+              ref={index === i ? ref : null}
+            >
+              <img
+                className={
+                  index === i ?
+                    'active' :
+                    ''
+                }
+                src={cat.imageUrl}
+                alt={'Cat #' + cat.id}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
+}
+
+const catList = [];
+for (let i = 0; i < 10; i++) {
+  catList.push({
+    id: i,
+    imageUrl: 'https://loremflickr.com/250/200/cat?lock=' + i
+  });
 }
