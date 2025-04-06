268149b95d9509df5cf3fd4a32182230ffaeb21b
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-04-06 22:23:43 +0900

    チャレンジ問題2（自分の回答）

--- src/App.js
+++ src/App.js
@@ -1,38 +1,22 @@
 import './App.css';
 import './referencing-values-with-refs/challenges3.css';
-import { useState, useRef } from 'react';
+import { useRef } from 'react';
 
-export default function VideoPlayer() {
-  const [isPlaying, setIsPlaying] = useState(false);
+export default function Page() {
   const ref = useRef(null);
 
   function handleClick() {
-    const nextIsPlaying = !isPlaying;
-    setIsPlaying(nextIsPlaying);
-
-    if (nextIsPlaying) {
-      ref.current.play();
-    } else {
-      ref.current.pause();
-    }
+    ref.current.focus();
   }
 
   return (
-    <>
-      <button onClick={handleClick}>
-        {isPlaying ? 'Pause' : 'Play'}
-      </button>
-      <video
-        width="250"
-        ref={ref}
-        onPlay={() => setIsPlaying(true)}
-        onPause={() => setIsPlaying(false)}
-      >
-        <source
-          src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
-          type="video/mp4"
-        />
-      </video>
+    <>  
+      <nav>
+        <button onClick={handleClick}>Search</button>
+      </nav>
+      <input ref={ref}
+        placeholder="Looking for something?"
+      />
     </>
-  )
+  );
 }
