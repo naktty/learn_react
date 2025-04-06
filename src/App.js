c376a2412acb69fe467f3ef0eaed67a18ce429af
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-04-06 22:11:34 +0900

    チャレンジ問題1（自分の回答）

--- src/App.js
+++ src/App.js
@@ -2,31 +2,41 @@ import './App.css';
 import './referencing-values-with-refs/challenges3.css';
 import { useState, useRef } from 'react';
 
-export default function Chat() {
-  const [text, setText] = useState('');
-  const textRef = useRef(text);
+export default function VideoPlayer() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const videoRef = useRef(null);
 
-  function handleChange(e) {
-    setText(e.target.value);
-    textRef.current = e.target.value;
+  function handleClick() {
+    const nextIsPlaying = !isPlaying;
+    setIsPlaying(nextIsPlaying);
+    
+    videoRef.current.controls = true;
+    if (nextIsPlaying) {
+      videoRef.current.play();
+    } else {
+      videoRef.current.pause();
+    }
   }
 
-  function handleSend() {
-    setTimeout(() => {
-      alert('Sending: ' + textRef.current);
-    }, 3000);
-  }
+  videoRef.onplay = (event) => {
+    setIsPlaying(false);
+  };
+
+  videoRef.onpause  = (event) => {
+    setIsPlaying(true);
+  };
 
   return (
     <>
-      <input
-        value={text}
-        onChange={handleChange}
-      />
-      <button
-        onClick={handleSend}>
-        Send
+      <button onClick={handleClick}>
+        {isPlaying ? 'Pause' : 'Play'}
       </button>
+      <video ref={videoRef} width="250">
+        <source 
+          src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
+          type="video/mp4"
+        />
+      </video>
     </>
-  );
+  )
 }
