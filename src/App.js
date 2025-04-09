07550a779f72495049061fee15d552a692e6510a
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-04-09 17:50:23 +0900

    チャレンジ問題2（模範 回答）

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import { useEffect, useRef } from 'react';
 export default function MyInput({ shouldFocus, value, onChange }) {
   const ref = useRef(null);
 
-  // TODO: call focus() only if shouldFocus is true.
   useEffect(() => {
-    shouldFocus && ref.current.focus();
+    if (shouldFocus) {
+      ref.current.focus();
+    }
   }, [shouldFocus]);
 
   return (
